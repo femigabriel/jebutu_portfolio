00e9265d4267ba3d17475b0b4f84020a51a15b3a
femigab <oluwafemi.gabriel@charisol.io>
2023-05-09 12:49:47 +0100

    contact update

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -15,7 +15,7 @@ export default function Contacts() {
           <span className="text-gray-400"> jebutublessing@gmailcom</span>
         </p>
         <p className="text-[12px] w-full">
-          <span className=" text-gray-600 w-full   mr-3">
+          <span className=" text-gray-600 w-full   mr-2">
             <span className="font-semibold mr-2"> Phone Numbers :</span>
             <span className="text-gray-400">
               +2348165655677, +2348081977497
